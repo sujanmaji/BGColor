06dd1a2758fd2e82325611c829bcd9a1c57a7b76
sujanmaji <sujanmaji948@gmail.com>
2021-09-24 01:47:36 +0530

    Created script.js and reduced code by creating ColorGenerator class

---
+++ script.js
@@ -0,0 +1,41 @@
+document.body.style.textAlign = "center";
+
+class ColorGenerator {
+    ColorGenerator() {
+    }
+    RGBColor() {
+        this.r = randBetween(0, 255);
+        this.g = randBetween(0, 255);
+        this.b = randBetween(0, 255);
+        return "rgb(" + this.r + "," + this.g + "," + this.b + ")";
+    }
+    rgbCode() {
+        return [this.r, this.g, this.b];
+    }
+}
+
+function randBetween(min, max) {
+    return Math.floor((max - min) * Math.random() + min);
+}
+
+let c = new ColorGenerator();
+function myFun() {
+    let mainDiv = document.body;
+    let randColor = c.RGBColor();
+    console.log(randColor);
+    mainDiv.style.background = randColor;
+}
+
+window.onload = myFun();
+
+document.addEventListener("click", function () {
+    myFun();
+    let ptn = c.rgbCode();
+    console.log(ptn);
+    navigator.vibrate(ptn);
+    //Deleting msg div
+    let del = document.getElementById("msgDiv");
+    if (del) {
+        del.remove();
+    }
+});
